868b451745c72904c7f3189adbe2d63ececf27b8
Utit Sairat <soodteeruk@gmail.com>
2013-04-05 13:13:58 +0700

    Create report layout and Add report menu form

---
+++ application/views/report_layout.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="pragma" content="no-cache" />
+
+    <title>Health Information System Report</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le styles -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/font-awesome-ie7.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap-timepicker.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/docs.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/datepicker.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/alertify.core.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/alertify.default.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/freeow/freeow.css" rel="stylesheet">
+    <style>
+        body {
+            padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+        }
+    </style>
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <script src="<?php echo base_url(); ?>assets/js/html5.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/head.load.min.js"></script>
+    <script>
+        head.js(
+                '<?php echo base_url(); ?>assets/js/bootstrap.min.js',
+                '<?php echo base_url(); ?>assets/js/underscore.min.js',
+                '<?php echo base_url(); ?>assets/js/taffy.js',
+                '<?php echo base_url(); ?>assets/js/bootstrap-datepicker.js',
+                '<?php echo base_url(); ?>assets/js/bootstrap-datepicker.th.js',
+                '<?php echo base_url(); ?>assets/js/bootstrap-datetimepicker.min.js',
+                '<?php echo base_url(); ?>assets/js/locales/bootstrap-datetimepicker.th.js',
+                '<?php echo base_url(); ?>assets/js/bootstrap-timepicker.min.js',
+                '<?php echo base_url(); ?>assets/js/jquery.blockUI.js',
+                '<?php echo base_url(); ?>assets/js/jquery.cookie.js',
+                '<?php echo base_url(); ?>assets/js/jquery.freeow.min.js',
+                '<?php echo base_url(); ?>assets/js/bootbox.min.js',
+                '<?php echo base_url(); ?>assets/js/jquery.maskedinput.min.js',
+                '<?php echo base_url(); ?>assets/js/typeahead.js',
+                '<?php echo base_url(); ?>assets/js/spin.min.js',
+                '<?php echo base_url(); ?>assets/js/jquery.paging.min.js',
+                '<?php echo base_url(); ?>assets/js/jquery.numeric.js',
+                '<?php echo base_url(); ?>assets/js/numeral.min.js'
+        );
+
+    </script>
+
+    <script src="<?php echo base_url(); ?>assets/apps/js/apps.js"></script>
+
+    <script type="text/javascript">
+        var csrf_token = '<?php echo $this->security->get_csrf_hash(); ?>';
+    </script>
+</head>
+
+<body>
+
+<div id="freeow" class="freeow freeow-bottom-right"></div>
+<div id="confirmmodalcontainer"></div>
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="#">EHIS <img id="imgLoading" style="display: none;" src="<?php echo base_url(); ?>assets/apps/img/ajax-loader-fb.gif" alt="Loading..."></a>
+
+            <div class="nav-collapse collapse">
+                <ul class="nav">
+                    <li>
+                        <a href="<?php echo site_url(); ?>">
+                            <i class="icon-home"></i>
+                            หน้าหลัก
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo site_url('reports/menu/1'); ?>">
+                            <i class="icon-signal"></i>
+                            การให้บริการ
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo site_url('reports/menu/2'); ?>">
+                            <i class="icon-signal"></i>
+                            งานส่งเสริม
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo site_url('reports/menu/3'); ?>">
+                            <i class="icon-signal"></i>
+                            ข้อมูลพื้นฐาน
+                        </a>
+                    </li>
+                </ul>
+                <div class="btn-group pull-right">
+                    <a href="#" class="btn dropdown-toggle" data-toggle="dropdown">
+                        <i class="icon-user"></i>
+                        <?php echo $this->session->userdata('fullname'); ?>
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="nav-header">USER PROFILES</li>
+                        <li>
+                            <a href="#"><i class="icon-edit"></i> เปลี่ยนรหัสผ่าน (Change password)</a>
+                        </li>
+                        <li>
+                            <a href="#"> <i class="icon-info-sign"></i> แก้ไขข้อมูลส่วนตัว</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo site_url('users/logout'); ?>"><i class="icon-off"></i> ออกจากระบบ (Logout)</a>
+                        </li>
+                    </ul>
+                </div>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <?php echo $content_for_layout; ?>
+</div> <!-- /container -->
+
+
+</body>
+</html>
---
+++ assets/apps/js/report.addreport.js
@@ -0,0 +1,227 @@
+/**
+ * Created By Mr.Utit Sairat.
+ * E-mail: soodteeruk@gmail.com
+ * Date: 4/4/2556 9:12 น.
+ */
+$(function() {
+    var rpt = {};
+
+    rpt.ajax = {
+        get_list: function(start, stop, cb) {
+            var url = 'reports/get_list',
+                params = {
+                    start: start,
+                    stop: stop,
+                    group: $('#tboGroup').val()
+                };
+
+            app.ajax(url, params, function(err, data){
+                err ? cb(err) : cb(null, data);
+            });
+        },
+        get_list_total: function(cb){
+            var url = 'reports/get_list_total',
+                params = {};
+
+            app.ajax(url, params, function(err, data){
+                err ? cb(err) : cb(null, data);
+            });
+        },
+        show_report: function() {
+            $('#mdlReport').modal('show').css({
+                width: 700,
+                'margin-left': function() {
+                    return -($(this).width() / 2);
+                }
+            });
+        },
+        hide_report: function() {
+            $('#mdlReport').modal('hide');
+        },
+        clear_form: function() {
+            $('#tboId').val('0');
+            $('#tboName').val('');
+            $('#tboUrl').val('');
+            $('#cboGroup').val('1');
+        },
+        load_item: function(id) {
+            app.ajax('reports/load_item', { id: id }, function(err, data) {
+                if(data != null) {
+                    if(_.size(data.rows)) {
+                        var v = data.rows[0];
+                        $('#tboId').val(id);
+                        $('#tboName').val(v.name);
+                        $('#tboUrl').val(v.url);
+                        $('#cboGroup').val(v.group);
+                    }
+                } else {
+                    app.alert(err);
+                }
+            });
+
+        }
+    };
+
+    $('#btnAddReport').click(function() {
+        rpt.ajax.clear_form();
+        var id = $(this).attr('data-id');
+        $('#cboGroup').val(id);
+
+        rpt.ajax.show_report();
+    });
+
+    $('#btnSave').click(function() {
+        var url = 'reports/save_report',
+            params = {
+                id: $('#tboId').val(),
+                name: $('#tboName').val(),
+                url: $('#tboUrl').val(),
+                group: $('#cboGroup').val()
+            };
+
+        if(params.name == '' || params.url == '') {
+            app.alert('กรุณากรอกข้อมูลให้ครบด้วย');
+        } else {
+            app.ajax(url, { data: params }, function(err, data) {
+                if(data != null) {
+                    app.alert(data.msg);
+                    rpt.ajax.hide_report();
+                    rpt.get_list();
+                } else {
+                    app.alert(err);
+                }
+            });
+        }
+    });
+
+    $(document).on('click', '#btnEdit', function() {
+        var id = $(this).attr('data-id');
+
+        rpt.ajax.clear_form();
+        rpt.ajax.load_item(id);
+        rpt.ajax.show_report();
+    });
+
+    $(document).on('click', '#btnRemove', function() {
+        var id = $(this).attr('data-id');
+
+        app.confirm('ยืนยันการลบเมนู ?', function(e) {
+            if(e) {
+                app.ajax('reports/remove_report', { id: id }, function(err, data) {
+                    if(data != null) {
+                        app.alert(data.msg);
+                        rpt.get_list();
+                    } else {
+                        app.alert(err);
+                    }
+                });
+            }
+        });
+    });
+
+    rpt.set_list = function(data){
+        if(_.size(data.rows) > 0){
+            _.each(data.rows, function(v){
+                $('#tblList > tbody').append(
+                    '<tr>'+
+                        '<td>'+ v.name +'</td>'+
+                        '<td>'+ v.url +'</td>'+
+                        '<td>'+
+                            '<div class="btn-group">'+
+                                '<a class="btn btn-success" data-id="'+ v.id +'" id="btnEdit" title="แก้ไขเมนู"><i class="icon-edit"></i></a>'+
+                                '<a class="btn btn-danger" data-id="'+ v.id +'" id="btnRemove" title="ลบเมนู"><i class="icon-trash"></i></a>'+
+                            '</div>'+
+                        '</td>'+
+                    '</tr>'
+                );
+            });
+        }else{
+            $('#tblList > tbody').append(
+                '<tr><td colspan="3">ไม่พบรายการ</td></tr>'
+            );
+        }
+    };
+    rpt.get_list = function(){
+        $('#main_paging').fadeIn('slow');
+        rpt.ajax.get_list_total(function(err, data){
+            if(err){
+                app.alert(err);
+            }else{
+                $('#main_paging > ul').paging(data.total, {
+                    format: " < . (qq -) nnncnnn (- pp) . >",
+                    perpage: app.record_per_page,
+                    lapping: 1,
+                    page: 1,
+                    onSelect: function(page){
+                        rpt.ajax.get_list(this.slice[0], this.slice[1], function(err, data){
+                            $('#tblList > tbody').empty();
+                            if(err){
+                                app.alert(err);
+                            }else{
+                                rpt.set_list(data);
+                            }
+
+                        });
+
+                    },
+                    onFormat: function(type){
+                        switch (type) {
+
+                            case 'block':
+
+                                if (!this.active)
+                                    return '<li class="disabled"><a href="">' + this.value + '</a></li>';
+                                else if (this.value != this.page)
+                                    return '<li><a href="#' + this.value + '">' + this.value + '</a></li>';
+                                return '<li class="active"><a href="#">' + this.value + '</a></li>';
+
+                            case 'right':
+                            case 'left':
+
+                                if (!this.active) {
+                                    return "";
+                                }
+                                return '<li><a href="#' + this.value + '">' + this.value + '</a></li>';
+
+                            case 'next':
+
+                                if (this.active) {
+                                    return '<li><a href="#' + this.value + '">&raquo;</a></li>';
+                                }
+                                return '<li class="disabled"><a href="">&raquo;</a></li>';
+
+                            case 'prev':
+
+                                if (this.active) {
+                                    return '<li><a href="#' + this.value + '">&laquo;</a></li>';
+                                }
+                                return '<li class="disabled"><a href="">&laquo;</a></li>';
+
+                            case 'first':
+
+                                if (this.active) {
+                                    return '<li><a href="#' + this.value + '">&lt;</a></li>';
+                                }
+                                return '<li class="disabled"><a href="">&lt;</a></li>';
+
+                            case 'last':
+
+                                if (this.active) {
+                                    return '<li><a href="#' + this.value + '">&gt;</a></li>';
+                                }
+                                return '<li class="disabled"><a href="">&gt;</a></li>';
+
+                            case 'fill':
+                                if (this.active) {
+                                    return '<li class="disabled"><a href="#">...</a></li>';
+                                }
+                        }
+                        return ""; // return nothing for missing branches
+                    }
+                });
+            }
+        });
+    };
+
+    rpt.get_list();
+});
