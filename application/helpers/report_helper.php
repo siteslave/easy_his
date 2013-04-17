6cc6463f80b04fa6fb78c823c5badb52566235ef
Utit Sairat <soodteeruk@gmail.com>
2013-04-17 13:54:35 +0700

    Update main report menu

--- application/helpers/report_helper.php
+++ application/helpers/report_helper.php
@@ -4,13 +4,35 @@
  * E-mail: soodteeruk@gmail.com
  * Date: 5/4/2556 14:03 น.
  */
-    if(!function_exists('get_main_report_menu')) {
-        function get_main_report_menu() {
-            $ci =& get_instance();
-            $ci->load->model('Report_model', 'report');
+if(!function_exists('get_main_report_menu')) {
+    function get_main_report_menu() {
+        $ci =& get_instance();
+        $ci->load->model('Report_model', 'report');
 
-            $rs = $ci->report->get_mainmenu();
+        $rs = $ci->report->get_mainmenu();
 
-            return $rs;
-        }
+        return $rs;
     }
+}
+
+if(!function_exists('get_main_report_menu_count')) {
+    function get_main_report_menu_count() {
+        $ci =& get_instance();
+        $ci->load->model('Report_model', 'report');
+
+        $rs = $ci->report->get_mainmenu_count();
+
+        return $rs;
+    }
+}
+
+if(!function_exists('get_other_mainmenu')) {
+    function get_other_mainmenu() {
+        $ci =& get_instance();
+        $ci->load->model('Report_model', 'report');
+
+        $rs = $ci->report->get_other_mainmenu();
+
+        return $rs;
+    }
+}
