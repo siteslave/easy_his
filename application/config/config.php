4f188d014ebff10082083b40e4c2ef542d61d76a
Satit Rianpit <siteslave@msn.com>
2013-07-17 07:05:02 +0000

    few update

--- application/config/config.php
+++ application/config/config.php
@@ -253,7 +253,7 @@ $config['encryption_key'] = 'sfsdfsdfdWsdfljsdlfsd00.s1fsdf4sd5fsd12fsdfw2efd#$%
 |
 */
 $config['sess_cookie_name']		= 'easyhis_session';
-$config['sess_expiration']		= 9999;
+$config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
--- assets/apps/js/apps.surveil.js
+++ assets/apps/js/apps.surveil.js
@@ -103,7 +103,7 @@ head.ready(function(){
         surveil.ajax.get_list_total(visit_date, function(err, data){
             if(err){
                 app.alert(err);
-                $('#tbl_list > tbody').append('<tr><td colspan="7">ไม่พบข้อมูล</td></td></tr>')
+                $('#tbl_list > tbody').append('<tr><td colspan="7">ไม่พบข้อมูล</td></td></tr>');
             }else{
                 $('#main_paging').paging(data.total, {
                     format: " < . (qq -) nnncnnn (- pp) . >",
@@ -115,7 +115,7 @@ head.ready(function(){
                         surveil.ajax.get_list(visit_date, this.slice[0], this.slice[1], function(err, data){
                             if(err){
                                 app.alert(err);
-                                $('#tbl_list > tbody').append('<tr><td colspan="7">ไม่พบข้อมูล</td></td></tr>')
+                                $('#tbl_list > tbody').append('<tr><td colspan="7">ไม่พบข้อมูล</td></td></tr>');
                             }else{
                                 surveil.set_list(data);
                             }
